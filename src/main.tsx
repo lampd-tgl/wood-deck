02cc34caf69f37d999eb6f33a09611ba846fb496
lampd-tgl <lam@tgl-sol.com>
2024-07-25 14:30:58 +0700

    update ver1.0.18

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { WoodDeck } from 'https://cdn.jsdelivr.net/gh/lampd-tgl/wood-deck@main/dist/wood-deck@1.0.16.js'
+// import { WoodDeck } from 'https://cdn.jsdelivr.net/gh/lampd-tgl/wood-deck@main/dist/wood-deck@1.0.17.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
